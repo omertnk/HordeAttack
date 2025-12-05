6823bd37c15997c7d572239494045cfae6bd3900
Ömer Faruk Tanık <omertank38@gmail.com>
2025-12-05 19:59:20 +0300

    Implemented Character Locomotion

---
+++ Assets/Prefabs/Character/Scripts/PlayerLocomotion.cs
@@ -0,0 +1,156 @@
+using System;
+using UnityEngine;
+
+public class PlayerLocomotion : MonoBehaviour
+{
+    PlayerManager playerManager;
+    AnimatorManager animatorManager;
+    InputManager inputManager;
+
+    Vector3 moveDirection;
+    Transform cameraObject;
+    Rigidbody playerRigidbody;
+
+    [Header("Falling")] public float inAirTimer;
+    public float leapingVelocity;
+    public float fallingVelocity;
+    public float rayCastHeightOffset = 0.5f;
+    public LayerMask groundLayer;
+
+    [Header("Movement Settings")] 
+    public bool isGrounded;
+    public bool isJumping;
+    
+    [Header("Movement Speeds")]
+    public float movementSpeed = 7;
+    public float rotationSpeed = 15;
+    
+    [Header("Jump Speeds")]
+    public float jumpHeight = 3;
+    public float gravityIntensity = -15;
+    
+
+    private void Awake()
+    {
+        isGrounded = true;
+        playerManager = GetComponent<PlayerManager>();
+        animatorManager = GetComponent<AnimatorManager>();
+        inputManager = GetComponent<InputManager>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        cameraObject = Camera.main.transform;
+    }
+
+    public void HandleAllMovement()
+    {
+        HandleFallingAndLanding();
+
+        if (playerManager.isInteracting)
+            return;
+
+        HandleMovement();
+        HandleRotation();
+    }
+
+    private void HandleMovement()
+    {
+        if (isJumping) return;
+        moveDirection = cameraObject.forward * inputManager.verticalInput;
+        moveDirection += moveDirection + cameraObject.right * inputManager.horizontalInput;
+        moveDirection.Normalize();
+        moveDirection.y = 0;
+        moveDirection *= movementSpeed;
+        
+        if (isGrounded && !isJumping)
+        {
+            Vector3 movementVelocity = moveDirection;
+            playerRigidbody.linearVelocity = movementVelocity;
+        }
+    }
+
+    private void HandleRotation()
+    {
+        if (isJumping) return;
+        Vector3 targetDirection = Vector3.zero;
+
+        targetDirection = cameraObject.forward * inputManager.verticalInput;
+        targetDirection = targetDirection + cameraObject.right * inputManager.horizontalInput;
+        targetDirection.Normalize();
+        targetDirection.y = 0;
+
+        if (targetDirection == Vector3.zero)
+            targetDirection = transform.forward;
+
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        Quaternion playerRotation =
+            Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        transform.rotation = playerRotation;
+    }
+
+    private void HandleFallingAndLanding()
+    {
+        RaycastHit hit;
+        Vector3 rayCastOrigin = transform.position;
+        Vector3 targetPosition;
+        rayCastOrigin.y += rayCastHeightOffset;
+        targetPosition = transform.position;
+        float rayDistance = rayCastHeightOffset + 0.5f;
+
+        bool isHit = Physics.SphereCast(rayCastOrigin, 0.2f, Vector3.down, out hit, rayDistance, groundLayer);
+
+        if (!isGrounded && !isHit && !isJumping)
+        {
+            if (!playerManager.isInteracting)
+            {
+                animatorManager.PlayTargetAnimation("Falling", true);
+            }
+
+            inAirTimer = inAirTimer + Time.deltaTime;
+            playerRigidbody.AddForce(transform.forward * leapingVelocity);
+            playerRigidbody.AddForce(-Vector3.up * (fallingVelocity * inAirTimer));
+        }
+
+        if (isHit)
+        {
+            if (!isGrounded && playerManager.isInteracting)
+            {
+                animatorManager.PlayTargetAnimation("Landing", true);
+            }
+            
+            Vector3 rayCastHitPoint = hit.point;
+            targetPosition.y = rayCastHitPoint.y;
+            inAirTimer = 0;
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+
+        if (isGrounded && !isJumping)
+        {
+            if (playerManager.isInteracting || inputManager.moveAmount > 0)
+            {
+                transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime / 0.1f);
+            }
+            else
+            {
+                transform.position = targetPosition;
+            }
+        }
+    }
+
+    public void HandleJumping()
+    {
+        if (isGrounded)
+        {
+            animatorManager.animator.SetBool("isJumping", true);
+            animatorManager.PlayTargetAnimation("Jump", false);
+            
+            float jumpingVelocity = Mathf.Sqrt(-2 * gravityIntensity * jumpHeight);
+            Vector3 playerVelocity = moveDirection;
+            playerVelocity.y = jumpingVelocity;
+            playerRigidbody.linearVelocity = playerVelocity;
+        }
+    }
+}
---
+++ Assets/Prefabs/Character/Scripts/PlayerManager.cs
@@ -0,0 +1,39 @@
+using System;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    Animator animator;
+    InputManager inputManager;
+    CameraManager cameraManager;
+    PlayerLocomotion playerLocomotion;
+
+    public bool isInteracting;
+    
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        inputManager = GetComponent<InputManager>();
+        cameraManager = FindAnyObjectByType<CameraManager>();
+        playerLocomotion = GetComponent<PlayerLocomotion>();
+    }
+
+    private void Update()
+    {
+        inputManager.HandleAllInputs();
+    }
+
+    private void FixedUpdate()
+    {
+        playerLocomotion.HandleAllMovement();
+    }
+
+    private void LateUpdate()
+    {
+        cameraManager.HandleAllCameraMovement();
+        
+        isInteracting = animator.GetBool("isInteracting");
+        playerLocomotion.isJumping = animator.GetBool("isJumping");
+        animator.SetBool("isGrounded", playerLocomotion.isGrounded);
+    }
+}
